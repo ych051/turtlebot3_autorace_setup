d677a3893f660ec0bc40809a7e5a643fd08e614b
ych051 <chanchan200510045@gmail.com>
2025-08-27 02:38:47 +0900

    Update wifiset.sh

--- wifiset.sh
+++ wifiset.sh
@@ -1,8 +1,18 @@
+echo "PRESS [ENTER] TO CONTINUE THE INSTALLATION"
+echo "IF YOU WANT TO CANCEL, PRESS [CTRL] + [C]"
+read
+
 echo "[Update the package lists]"
 
-sudo apt update -y
+sudo apt update
 sudo apt upgrade -y
+
+echo "[Wifi Setting Start]"
+
+sudo apt install build-essential
+
 git clone https://github.com/HRex39/rtl8852be.git
+
 cd $HOME/rtl8852be
 make -j8
 sudo make install
